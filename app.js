686a8611c83d1bea0198b0d072eda053299a42aa
carrz <carla.zumaran@gmail.com>
2018-02-26 09:56:46 -0300

    Creando repositorio con ejemplo API de paypal funcionando

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AY6oDn_X3iJYG9f1j4A5yrKY2_za4trfukqRFUfOs2JaNMA81zgE8DwYmoXtdaTKfs3dLL8slMLUXR4e',
+  'client_secret': 'EKS9vwXERx8TiURYt9Hp5nIGC8uDaL4l2Vyipw7lJ77IuG01CrFvqTjMX19QbY40VwXQjg2b9ypFQQCJ'
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/pay', (req, res) => {
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+      "payment_method": "paypal"
+    },
+    "redirect_urls": {
+      "return_url": "http://localhost:3000/success",
+      "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+      "item_list": {
+        "items": [{
+          "name": "Red Sox Hat",
+          "sku": "001",
+          "price": "250.00",
+          "currency": "USD",
+          "quantity": 1
+        }]
+      },
+      "amount": {
+        "currency": "USD",
+        "total": "250.00"
+      },
+      "description": "Hat for the best team ever"
+    }]
+  };
+
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === 'approval_url') {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+
+});
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+      "amount": {
+        "currency": "USD",
+        "total": "250.00"
+      }
+    }]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      res.send('Success');
+    }
+  });
+});
+
+app.get('/cancel', (req, res) => res.send('Cancelled'));
+
+app.listen(3000, () => console.log('Server Started'));
