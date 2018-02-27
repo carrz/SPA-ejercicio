c1c497699f8121820c430e7b2669ed846874528f
Pauliih <pau.campos89@gmail.com>
2018-02-27 11:14:02 -0300

    + routing

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -25,7 +25,7 @@ categoryId.forEach(idCategory => {
         let data = response.children_categories;
         data.forEach(element => {
           $(`#${category}`).append(
-            `<li id="${element.id}" class="subcategory"><a href="#">${element.name}</a></li>`);
+            `<li id="${element.id}" class="subcategory"><a href="/subcategory/${element.id}">${element.name}</a></li>`);
         })
         $('.subcategory').unbind('click');
         $('.subcategory').on('click', (e) => {
@@ -87,8 +87,11 @@ function renderProducts(element) {
 
 //------EVENTO BARRA DE BÚSQUEDA---------//
 $('#search').keydown(function(event) {
+
   if (event.which == 13) {
     let search = $('#search').val();
+    // routing to search page
+    page(`/search?=${search}`)
     event.preventDefault();
     fetchBySearch(function(response) {
       let data = response.results;
@@ -118,34 +121,12 @@ function fetchBySearch(callback, errorCallback, search) {
     })
 }
 
-  // This function is called only once - on page load.
-  // It fills up the products list via a handlebars template.
-  // It recieves one parameter - the data we took from products.json.
-  // function generateAllProductsHTML(data){
-
-  //   var list = $('.all-products .products-list');
-
-  //   var theTemplateScript = $("#products-template").html();
-  //   //Compile the template​
-  //   var theTemplate = Handlebars.compile (theTemplateScript);
-  //   list.append (theTemplate(data));
-
-
-  //   // Each products has a data-index attribute.
-  //   // On click change the url hash to open up a preview for this product only.
-  //   // Remember: every hashchange triggers the render function.
-  //   list.find('li').on('click', function (e) {
-  //     e.preventDefault();
-
-  //     var productIndex = $(this).data('index');
-
-  //     window.location.hash = 'product/' + productIndex;
-  //   })
-  // }
-
   //--------------- USO DE page.js ------------------//
+  // Configuración page.js: Manejar navegacion entre rutas
+  // Formato: page('ruta', callback);
   page('/', index)
-  page('/search', searchBar)
+  // page('/subcategory/${element.id}', subcategory)
+  // page('/search', searchBar)
   page('/cart', cart)
   page('/item', item)
   page('*', notFound)
@@ -153,22 +134,26 @@ function fetchBySearch(callback, errorCallback, search) {
 
   // Page index
   function index() {
-    document.querySelector('.index-content').innerHTML = '<!-- Jumbotron Header -->' +
-      '<div class="container">' +
-        '<div class="row">' +
-          '<div class="jumbotron text-center my-4">' +
-            '<h1 class="display-3">A Warm Welcome!</h1>' +
-            '<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>' +
-          '</div>' +
-        '</div>' +
-      '</div>'
+    $('.index-content').empty();
+    $('.index-content').append(`
+      <!-- Jumbotron Header -->
+      <div class="container">
+        <div class="row">
+          <div class="jumbotron text-center my-4">
+            <h1 class="display-3">A Warm Welcome!</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+          </div>
+        </div>
+      </div>`)
   }
 
-  function searchBar() {
+  // function subcategory() {}
+
+  // function searchBar() {
     // renderProducts(element);
     // document.querySelector('.index-content .row').append(`
     //   `);
-  }
+  // }
 
   // page cart
   function cart() {
@@ -256,79 +241,80 @@ function fetchBySearch(callback, errorCallback, search) {
     // agregar informacion de un producto
     $('.index-content .container .row').append(`<h2>Nombre del Producto</h2>
       <div class="container">
-    <div class="row">
-        <!-- Image -->
-        <div class="col-12 col-lg-6">
-            <div class="card bg-light mb-3">
-                <div class="card-body">
-                    <a href="" data-toggle="modal" data-target="#productModal">
-                        <img class="img-fluid" src="https://dummyimage.com/800x800/55595c/fff">
-                        <p class="text-center">Zoom</p>
-                    </a>
-                </div>
-            </div>
-        </div>
+        <div class="row">
+          <!-- Image -->
+          <div class="col-12 col-lg-6">
+              <div class="card bg-light mb-3">
+                  <div class="card-body">
+                      <a href="" data-toggle="modal" data-target="#productModal">
+                          <img class="img-fluid" src="https://dummyimage.com/800x800/55595c/fff">
+                          <p class="text-center">Zoom</p>
+                      </a>
+                  </div>
+              </div>
+          </div>
 
-        <!-- Add to cart -->
-        <div class="col-12 col-lg-6 add_to_cart_block">
-            <div class="card bg-light mb-3">
-                <div class="card-body">
-                    <p class="price">99.00 $</p>
-                    <p class="price_discounted">149.90 $</p>
-                    <form method="get" action="cart.html">
-                        <div class="form-group">
-                            <label for="colors">Color</label>
-                            <select class="custom-select" id="colors">
-                                <option selected="">Select</option>
-                                <option value="1">Blue</option>
-                                <option value="2">Red</option>
-                                <option value="3">Green</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>Quantity :</label>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <button type="button" class="quantity-left-minus btn btn-danger btn-number" data-type="minus" data-field="">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                </div>
-                                <input type="text" class="form-control" id="quantity" name="quantity" min="1" max="100" value="1">
-                                <div class="input-group-append">
-                                    <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="cart.html" class="btn btn-success btn-lg btn-block text-uppercase">
-                            <i class="fa fa-shopping-cart"></i> Add To Cart
-                        </a>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+          <!-- Add to cart -->
+          <div class="col-12 col-lg-6 add_to_cart_block">
+              <div class="card bg-light mb-3">
+                  <div class="card-body">
+                      <p class="price">99.00 $</p>
+                      <p class="price_discounted">149.90 $</p>
+                      <form method="get" action="cart.html">
+                          <div class="form-group">
+                              <label for="colors">Color</label>
+                              <select class="custom-select" id="colors">
+                                  <option selected="">Select</option>
+                                  <option value="1">Blue</option>
+                                  <option value="2">Red</option>
+                                  <option value="3">Green</option>
+                              </select>
+                          </div>
+                          <div class="form-group">
+                              <label>Quantity :</label>
+                              <div class="input-group mb-3">
+                                  <div class="input-group-prepend">
+                                      <button type="button" class="quantity-left-minus btn btn-danger btn-number" data-type="minus" data-field="">
+                                          <i class="fa fa-minus"></i>
+                                      </button>
+                                  </div>
+                                  <input type="text" class="form-control" id="quantity" name="quantity" min="1" max="100" value="1">
+                                  <div class="input-group-append">
+                                      <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                                          <i class="fa fa-plus"></i>
+                                      </button>
+                                  </div>
+                              </div>
+                          </div>
+                          <a href="cart.html" class="btn btn-success btn-lg btn-block text-uppercase">
+                              <i class="fa fa-shopping-cart"></i> Add To Cart
+                          </a>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
 
-    <div class="row">
-        <!-- Description -->
-        <div class="col-12">
-            <div class="card border-light mb-3">
-                <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-align-justify"></i> Description</div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-                    </p>
-                    <p class="card-text">
-                        Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`);
+      <div class="row">
+          <!-- Description -->
+          <div class="col-12">
+              <div class="card border-light mb-3">
+                  <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-align-justify"></i> Description</div>
+                  <div class="card-body">
+                      <p class="card-text">
+                          Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                      </p>
+                      <p class="card-text">
+                          Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>`);
   }
 
+  // Page not found
   function notFound() {
     $('.index-content .container .row').append('<p>notFound</p>');
   }
